22f4c89cb366861df133828c9c035b6d1e503e72
David Foote <david.a.foote@gmail.com>
2019-06-20 21:59:23 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@song_count = 0
+  
+  def initialize(name, artist, genre)
+    @@song_count += 1
+  end
+  
+  def self.count
+    @@song_count
+  end
+end
