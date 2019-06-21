4f287afe3444469d4206697c9d2f4a8e06cedc18
David Foote <david.a.foote@gmail.com>
2019-06-21 18:54:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,26 @@
 class Song
   attr_accessor :name, :artist, :genre
+  
   @@count = 0
   @@artists = []
   @@genres = []
+  @@artist_count = {}
+  @@genre_count = {}
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists << self
+    @@genres << self
   end
   
-  def self.count
-    @song_count = song_count
-  end
+  # def self.artist
+  #   @@artists
+  # end
   
-  def self.artists
-    @artist_count = artist_count
-  end
-  
-  def self.genres
-    @self.genres
-  end
+  # def self.genre
+  #   @@genres
+  # end
 end
