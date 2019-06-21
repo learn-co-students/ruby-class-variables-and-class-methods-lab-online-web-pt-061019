39102bfe8bfa6fd6cd7355bb767dc6d50267fe0b
David Foote <david.a.foote@gmail.com>
2019-06-21 19:05:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,23 +4,17 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  @@artist_count = {}
-  @@genre_count = {}
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count += 1
     @@artists << self
     @@genres << self
+    @@count += 1
   end
   
-  # def self.artist
-  #   @@artists
-  # end
-  
-  # def self.genre
-  #   @@genres
-  # end
-end
+  def self.count
+     @@count
+  end
+ end
