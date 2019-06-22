5ca5b9df2ed94497e2fb33da657fd52ab4c7d5bf
David Foote <david.a.foote@gmail.com>
2019-06-22 02:04:30 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,8 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    @@artists << self
-    @@genres << self
+    @@artists << artist
+    @@genres << genre
     @@count += 1
   end
   
@@ -18,8 +18,35 @@ class Song
      @@count
   end
   
-  @@artists = []
   def self.artists
     @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_count = {}
+    @@genres.collect {|genre| 
+    if genre_count[genre] 
+      genre_count[genre] += 1
+    else
+      genre_count[genre] = 1
+    end
+    }
+    genre_count
+  end
+  
+  def self.artist_count
+    artist_count = {}
+    @@artists.collect {|artist|
+    if artist_count[artist]
+      artist_count[artist] += 1
+    else
+      artist_count[artist] = 1
+    end
+    }
+    artist_count
   end
  end
