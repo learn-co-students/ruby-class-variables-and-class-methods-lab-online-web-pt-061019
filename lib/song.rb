019fbd3af5a739a491e05938c6c400798c6e6b83
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-24 03:18:34 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,21 @@
 class Song
- @@song_count = []
-  def initialize
+     attr_accessor("name", "artist", "genre")
+ @@song_count = 0
+  def initialize (name, artist, genre)
    @@song_count += 1
+   @name = name
+   @artist = artist
+   @genre = genre
   end
 
+song.name
   def song_count
     self.count
   end
+song1 = Song.new("song1","artist","genre")
+
+end
+
 end
 
 
@@ -15,6 +24,3 @@ end
 
   #The Song class can produce individual songs. Each song has a name,
   #an artist and a genre. We need our Song class to be able to keep track of the number of songs that it creates.
-
-
-end
