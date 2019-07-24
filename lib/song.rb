44894cf5d6fa72fb7d0ba1d7586416ec00a816d3
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-24 17:59:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,65 @@
+require'pry'
 class Song
-     attr_accessor("name", "artist", "genre")
- @@song_count = 0
+     attr_accessor(:name, :artist, :genre)
+ @@count = 0
+ @@artists = []
+ @@genres = []
   def initialize (name, artist, genre)
-   @@song_count += 1
-   @name = name
-   @artist = artist
-   @genre = genre
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
   end
 
-song.name
-  def song_count
-    self.count
+  def self.count
+    @@count
   end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+   def self.genre_count
+
+     genre_hash = {}
+    @@genres.each do |a|
+      #binding.pry
+
+      if genre_hash[a]
+        genre_hash[a] += 1
+      else
+         genre_hash[a] = 1
+      end
+    end
+    genre_hash
+   end
+
+   def self.artist_count
+
+     artist_hash = {}
+    @@artists.each do |a|
+      #binding.pry
+
+      if artist_hash[a]
+        artist_hash[a] += 1
+      else
+         artist_hash[a] = 1
+      end
+    end
+    artist_hash
+   end
+
+
+# You will need to iterate over the @@genres array and populate a hash with the key/value pairs. You will need to check to see if the hash already contains a key of a particular genre. If so, increment the value of that key by one, otherwise, create a new key/value pair.
+    #
 song1 = Song.new("song1","artist","genre")
 
+
+
 end
-
-end
-
-
-
-
-
-  #The Song class can produce individual songs. Each song has a name,
-  #an artist and a genre. We need our Song class to be able to keep track of the number of songs that it creates.
